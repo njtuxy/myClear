30276e3e66460be8199c72230477215d03268d7d
Yan Xia <yxia@kabaminc.com>
2015-01-18 18:06:39 -0800

    check point 3

--- ClearStyle/ViewController.swift
+++ ClearStyle/ViewController.swift
@@ -19,9 +19,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self;
-        tableView.delegate = self;
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(TableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorStyle = .None
+        tableView.rowHeight = 50.0
+        
+        
+        
         
         if(toDoItems.count>0)
         {
@@ -57,10 +62,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as TableViewCell
+
         let item = toDoItems[indexPath.row]
         cell.textLabel?.text = item.text
+        cell.textLabel?.backgroundColor = UIColor.clearColor()
         return cell
+    }
+    
+    func colorForIndex(index: Int) -> UIColor {
+        let itemCount = toDoItems.count - 1
+        let val = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: 1.0, green: val, blue: 0.0, alpha: 1.0)
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell,
+        forRowAtIndexPath indexPath: NSIndexPath) {
+            cell.backgroundColor = colorForIndex(indexPath.row)
     }
 
 
